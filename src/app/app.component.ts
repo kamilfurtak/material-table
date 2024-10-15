c8ed2590de9e1f28e3e701a05ec949ddfb9e236d
Kamil Furtak <kamil@furtak.dev>
2024-10-15 10:22:35 +0200

    disable backdrop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,43 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule, MatDialog } from '@angular/material/dialog';
-import { TableDialogComponent } from './table-dialog/table-dialog.component';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialog, MatDialogModule } from "@angular/material/dialog";
+import { TableDialogComponent } from "./table-dialog/table-dialog.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [CommonModule, MatButtonModule, MatDialogModule],
   template: `
     <h1>Angular Material Table Demo</h1>
-    <button mat-raised-button color="primary" (click)="openDialog()">Open Table Dialog</button>
+    <button mat-raised-button color="primary" (click)="openDialog()">
+      Open Table Dialog
+    </button>
   `,
-  styles: [`
-    :host {
-      display: block;
-      padding: 20px;
-    }
-  `]
+  styles: [
+    `
+      :host {
+        display: block;
+        padding: 20px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   constructor(public dialog: MatDialog) {}
 
   openDialog() {
     const dialogRef = this.dialog.open(TableDialogComponent, {
-      width: '80%',
-      height: '80%',
-      maxWidth: '100vw',
-      maxHeight: '100vh',
-      panelClass: 'draggable-dialog'
+      width: "80%",
+      height: "80%",
+      maxWidth: "100vw",
+      maxHeight: "100vh",
+      panelClass: "draggable-dialog",
+      hasBackdrop: false,
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log("The dialog was closed");
     });
   }
 }
