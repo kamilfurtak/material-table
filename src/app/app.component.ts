c962c9da44f595dcd47287047fd733f9a0377f04
Kamil Furtak <kamil@fcomp.dev>
2024-11-09 07:24:54 +0100

    add openlayers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,40 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatDialog, MatDialogModule } from "@angular/material/dialog";
 import { TableDialogComponent } from "./table-dialog/table-dialog.component";
 import { DialogRegistryService } from "./services/dialog-registry.service";
+import { AngularOpenlayersModule } from "ng-openlayers";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatDialogModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatDialogModule,
+    AngularOpenlayersModule,
+  ],
   template: `
     <h1>Angular Material Table Demo</h1>
     <button mat-raised-button color="primary" (click)="openDialog()">
       Open Table Dialog
     </button>
+    <div style="height: 500px">
+      <aol-map>
+        <aol-view [zoom]="2">
+          <aol-coordinate
+            [x]="5.795122"
+            [y]="45.210225"
+            [srid]="'EPSG:4326'"
+          ></aol-coordinate>
+        </aol-view>
+        <aol-layer-tile>
+          <aol-source-osm></aol-source-osm>
+        </aol-layer-tile>
+        <aol-interaction-default></aol-interaction-default>
+        <aol-control-scaleline></aol-control-scaleline>
+        <aol-control-zoomslider></aol-control-zoomslider>
+        <aol-control-zoom></aol-control-zoom>
+      </aol-map>
+    </div>
   `,
   styles: [
     `
