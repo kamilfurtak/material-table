421f1b35fdf1fbbd05e386eac45071645e2231d2
Kamil Furtak <kamil@fcomp.dev>
2024-11-11 15:12:49 +0100

    add publishable lib

---
+++ libs/material-components/src/lib/material-components/material-components.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-material-components',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './material-components.component.html',
+  styleUrl: './material-components.component.css',
+})
+export class MaterialComponentsComponent {}
