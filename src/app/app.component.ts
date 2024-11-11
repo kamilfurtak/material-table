266d30046f693ab3bd4f283df01c46ddc353f2bb
Kamil Furtak <kamil@fcomp.dev>
2024-11-11 07:50:15 +0100

    extract map component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,21 @@
 import { Component, OnInit } from "@angular/core";
-import { CommonModule } from "@angular/common";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { AngularOpenlayersModule } from "ng-openlayers";
 
 import { DialogService } from "./components/dialog/dialog.service";
+import { MapComponent } from "./map/map.component";
+import { MatTooltip } from "@angular/material/tooltip";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  providers: [DialogService],
   imports: [
-    CommonModule,
     MatButtonModule,
-    AngularOpenlayersModule,
     MatToolbarModule,
     MatIconModule,
-    MatTooltipModule,
+    MapComponent,
+    MatTooltip,
   ],
   template: `
     <mat-toolbar color="primary">
@@ -29,44 +26,8 @@ import { DialogService } from "./components/dialog/dialog.service";
       </button>
     </mat-toolbar>
 
-    <div class="map-container">
-      <aol-map>
-        <aol-view [zoom]="2">
-          <aol-coordinate
-            [x]="5.795122"
-            [y]="45.210225"
-            [srid]="'EPSG:4326'"
-          ></aol-coordinate>
-        </aol-view>
-        <aol-layer-tile>
-          <aol-source-osm></aol-source-osm>
-        </aol-layer-tile>
-        <aol-interaction-default></aol-interaction-default>
-        <aol-control-scaleline></aol-control-scaleline>
-        <aol-control-zoomslider></aol-control-zoomslider>
-        <aol-control-zoom></aol-control-zoom>
-      </aol-map>
-    </div>
+    <app-map></app-map>
   `,
-  styles: [
-    `
-      :host {
-        display: flex;
-        flex-direction: column;
-        height: 100vh;
-      }
-
-      .map-container {
-        flex: 1;
-        width: 100%;
-      }
-
-      aol-map {
-        width: 100%;
-        height: 100%;
-      }
-    `,
-  ],
 })
 export class AppComponent implements OnInit {
   constructor(private dialogService: DialogService) {}
